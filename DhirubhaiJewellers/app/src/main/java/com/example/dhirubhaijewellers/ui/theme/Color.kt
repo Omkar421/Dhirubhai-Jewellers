018714ef7b8efdfcb3d05070d8c15282d50ede53
Omkar421 <omkarpati420@gmail.com>
2026-04-17 16:04:39 +0530

    Add files via upload

---
+++ DhirubhaiJewellers/app/src/main/java/com/example/dhirubhaijewellers/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.dhirubhaijewellers.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GoldPrimary = Color(0xFFD4AF37)
+val GoldDark = Color(0xFF996515)
+val Charcoal = Color(0xFF121212)
+val OffWhite = Color(0xFFF5F5F5)
