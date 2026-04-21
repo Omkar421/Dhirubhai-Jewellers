bea9b78cabbc07abf32bcc3b5b7efb51dc00402d
Omkar421 <omkarpati420@gmail.com>
2026-04-21 10:42:15 +0530

    Delete DhirubhaiJewellers directory

--- DhirubhaiJewellers/app/src/main/java/com/example/dhirubhaijewellers/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dhirubhaijewellers.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val GoldPrimary = Color(0xFFD4AF37)
-val GoldDark = Color(0xFF996515)
-val Charcoal = Color(0xFF121212)
-val OffWhite = Color(0xFFF5F5F5)
